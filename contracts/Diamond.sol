a8c87533b705034188f501246e3deb1a446de4bb
Nick Mudge <nick@perfectabstractions.com>
2020-11-26 23:17:24 -0500

    Removed receive function from diamond

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -16,19 +16,25 @@ import "./interfaces/IERC173.sol";
 import "./interfaces/IERC165.sol";
 
 contract Diamond {
-    constructor(IDiamondCut.FacetCut[] memory _diamondCut, address _owner) payable {
+    // more arguments are added to this struct
+    // this avoids stack too deep errors
+    struct DiamondArgs {
+        address owner;
+    }
+
+    constructor(IDiamondCut.FacetCut[] memory _diamondCut, DiamondArgs memory _args) payable {
         LibDiamond.diamondCut(_diamondCut, address(0), new bytes(0));
-        LibDiamond.setContractOwner(_owner);
+        LibDiamond.setContractOwner(_args.owner);
 
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
-                        
+
         // adding ERC165 data
         ds.supportedInterfaces[type(IERC165).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
         ds.supportedInterfaces[type(IERC173).interfaceId] = true;
     }
-    
+
     // Find facet for function that is called and execute the
     // function if a facet is found and return any value.
     fallback() external payable {
@@ -52,6 +58,4 @@ contract Diamond {
                 }
         }
     }
-
-    receive() external payable {}
 }
